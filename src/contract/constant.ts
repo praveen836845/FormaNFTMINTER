1d1eb29c28faaa294db934731c17f393df953e43
Roy Singh <roysingh@Roys-MacBook-Air.local>
2025-03-05 22:28:31 +0530

    changes

--- src/contract/constant.ts
+++ src/contract/constant.ts
@@ -1,4 +1,4 @@
-import Contract_ABI from '../contract/Contract_ABI.json'
+import Contract_ABI from './Contract_ABI.json'
 
 export const CONTRACT_ADDRESS = "0x9Db4265f24E8C84439b0cd6a56731f0572828f36";
 export const CONTRACT_ABI = Contract_ABI;
